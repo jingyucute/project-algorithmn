dfd192bc93d493e68d54bdf358e0493e49c7ad3c
jingyu <jingyucute@gmail.com>
2020-08-22 18:31:29 +0800

    add(leetcode): 爬楼梯

---
+++ leetcode/70-climbStairs.py
@@ -0,0 +1,57 @@
+'''
+Description:
+Author: jingyu
+Date: 2020-08-14 01:09:27
+LastEditors: Please set LastEditors
+LastEditTime: 2020-08-22 18:29:32
+'''
+
+# 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+# 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+# 注意：给定 n 是一个正整数。
+
+# 示例 1：
+
+# 输入： 2
+# 输出： 2
+# 解释： 有两种方法可以爬到楼顶。
+# 1.  1 阶 + 1 阶
+# 2.  2 阶
+# 示例 2：
+
+# 输入： 3
+# 输出： 3
+# 解释： 有三种方法可以爬到楼顶。
+# 1.  1 阶 + 1 阶 + 1 阶
+# 2.  1 阶 + 2 阶
+# 3.  2 阶 + 1 阶
+
+#!user/bin/python
+# _*_ coding: utf-8 _*_
+import datetime
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+
+if __name__ == '__main__':
+    n = 5
+    start_time = datetime.datetime.now()
+    solution = Solution()
+    result = solution.climbStairs(n)
+    print(result)
+    end_time = datetime.datetime.now()
+    print(end_time-start_time)
