44e4912553fb635b6b7ff54dabf0dd82b03f5a73
jingyu <jingyucute@gmail.com>
2020-08-28 10:43:19 +0800

    add(leetcode): 路径总和

---
+++ leetcode/112-hasPathSum.py
@@ -0,0 +1,105 @@
+'''
+Description:
+Author: jingyu
+Date: 2020-08-14 01:09:27
+LastEditors: Please set LastEditors
+LastEditTime: 2020-08-28 10:43:50
+'''
+# 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+# 说明: 叶子节点是指没有子节点的节点。
+
+# 示例: 
+# 给定如下二叉树，以及目标和 sum = 22，
+
+#               5
+#              / \
+#             4   8
+#            /   / \
+#           11  13  4
+#          /  \      \
+#         7    2      1
+# 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+
+#!user/bin/python
+# _*_ coding: utf-8 _*_
+
+import datetime
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == sum
+        if self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val):
+            return True
+        return False
+
+    def broadTravse(self, root):
+        result = []
+        if not root:
+            return result
+        queue = []
+        queue.append(root)
+        while queue:
+            length = len(queue)
+            temp = []
+            for _ in range(length):
+                node = queue[0]
+                del queue[0]
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(temp[:])
+        return result
+
+
+if __name__ == '__main__':
+    root = TreeNode(1)
+    node1 = TreeNode(2)
+    node2 = TreeNode(5)
+    node1_1 = TreeNode(3)
+    node1_2 = TreeNode(4)
+    node2_1 = TreeNode(4)
+    node2_2 = TreeNode(3)
+    root.left = node1
+    root.right = node2
+    node1.left = node1_1
+    node1.right = node1_2
+    node2.left = node2_1
+    node2.right = node2_2
+
+    head = ListNode(-10)
+    lnode1 = ListNode(-3)
+    lnode2 = ListNode(0)
+    lnode3 = ListNode(5)
+    lnode4 = ListNode(9)
+    head.next = lnode1
+    lnode1.next = lnode2
+    lnode2.next = lnode3
+    lnode3.next = lnode4
+
+    start_time = datetime.datetime.now()
+    solution = Solution()
+    result = solution.hasPathSum(root, 9)
+    print(result)
+    end_time = datetime.datetime.now()
+    print(end_time-start_time)
